dd84e1578ba9ee79f4bcdf9f8e0678dcafac865a
Ghouroub <Ghouroub>
2016-07-29 10:23:33 +0300

    first commit

---
+++ first.js
@@ -0,0 +1,28 @@
+var x="abcde";
+var y="abfer";
+var count=0;
+if(x[0]==y[0])
+{
+count=count+1
+}
+if(x[1]==y[1])
+{
+count=count+1
+}
+if(x[2]==y[2])
+{
+count=count+1
+}
+if(x[3]==y[3])
+{
+count=count+1
+}
+if(x[4]==y[4])
+{
+count=count+1
+}
+if(count>=2)
+{
+console.log("true");
+alert("count is "+count);
+}
